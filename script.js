71c8acfda1f3cf1fd5b9f972c480f9bd6c82a3c4
Gabriel Galvez <gabriel.g.galvez1985@gmail.com>
2022-11-08 20:10:48 -0300

    fifth commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 //    dibujarTabla();
 //}
 
-(carrito.length != 0) && dibujarTabla();
+(carrito.length != 0)&&dibujarTabla();
 obtenerDolar();
 //LUXON
 const DateTime = luxon.DateTime;
